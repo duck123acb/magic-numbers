41867dd8f890369cb32d14d9651c4cc1179880fc
Danush Ramanan <dan.ramanan@gmail.com>
2024-07-06 09:18:40 -0600

    magic_number_search is overflowing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,50 @@
-fn generate_sliding_piece_mask(square: &i32, orthagonal: bool, diagonal: bool) -> u64 { // returns a bitboard with all the squares it attacks
+use std::collections::HashMap;
+use std::env;
+
+
+use rand::random;
+use rand::Rng;
+use rand::thread_rng;
+
+fn generate_sliding_piece_mask(square: &i32, orthagonal: bool, diagonal: bool) -> u64 {
+  let piece_bitboard = 1 << square;
+  let mut moves = 0;
+  let mut directions = Vec::new();
+
+  if orthagonal {
+    directions.push(8); // up
+    directions.push(-8); // down
+    directions.push(1); // left
+    directions.push(-1); // right
+  }
+  if diagonal {
+    directions.push(9); // up left
+    directions.push(7); // up right
+    directions.push(-9); // down right
+    directions.push(-7); // down left
+  }
+
+  for direction in directions {
+    for shift in 1..7 {
+      let new_square = if direction > 0 { 
+        piece_bitboard << shift * direction
+      } else {
+        piece_bitboard >> shift * (direction * -1)
+      };
+
+      // we only need to go to the second-whatever file because we are treating like everyting is a capture, so we can just add that move later in the move gen function
+      if new_square & (0xFF00000000000000 & 0x00000000000000FF & 0x8080808080808080 & 0x0101010101010101) != 0 { // second top rank
+        break;
+      }
+  
+      moves |= new_square;
+      
+    }
+  }
+
+  moves
+}
+fn find_legal_moves(square: &i32, occupation: &u64, orthagonal: bool, diagonal: bool) -> u64 {
   let piece_bitboard = 1 << square;
   let mut moves = 0;
   let mut directions = Vec::new();
@@ -25,18 +71,11 @@ fn generate_sliding_piece_mask(square: &i32, orthagonal: bool, diagonal: bool) -
       };
   
       moves |= new_square;
-      
-      // we only need to go to the second-whatever file because we are treating like everyting is a capture, so we can just add that move later
-      if new_square & (0xFF00000000000000 >> 8) != 0 { // second top rank
+
+      if new_square & occupation != 0 {
         break;
       }
-      if new_square & (0x00000000000000FF << 8) != 0 { // second bottom rank
-        break;
-      }
-      if new_square & (0x8080808080808080 >> 1) != 0 { // second left file
-        break;
-      }
-      if new_square & (0x0101010101010101 << 1) != 0 { // second right file
+      if new_square & (0xFF00000000000000 & 0x00000000000000FF & 0x8080808080808080 & 0x0101010101010101) != 0 { // second top rank
         break;
       }
     }
@@ -45,7 +84,11 @@ fn generate_sliding_piece_mask(square: &i32, orthagonal: bool, diagonal: bool) -
   moves
 }
 
-fn set_occupancy(index: u64, attack_mask: u64) -> u64 { // shift bits into the mask
+fn count_bits(bitboard: u64) -> u32 {
+  bitboard.count_ones()
+}
+
+fn set_occupancy(index: u64, attack_mask: &u64) -> u64 { // shift bits into the mask
   let mut occupancy:u64 = 0;
   let mut bit_index = 0;
 
@@ -60,11 +103,11 @@ fn set_occupancy(index: u64, attack_mask: u64) -> u64 { // shift bits into the m
 
   occupancy
 }
-fn generate_occupancies(attack_mask: u64) -> Vec<u64> {
+fn generate_occupancies(attack_mask: &u64) -> Vec<u64> {
   let mut occupancies = Vec::new();
 
   // Iterate through all possible bit combinations within the attack mask
-  for index in 0..(1 << attack_mask.count_ones()) { // 0 to 2^number of bits on
+  for index in 0..(1 << count_bits(*attack_mask)) { // 0 to 2^number of bits on
     let occupancy = set_occupancy(index, attack_mask);
     occupancies.push(occupancy);
   }
@@ -72,10 +115,47 @@ fn generate_occupancies(attack_mask: u64) -> Vec<u64> {
   occupancies
 }
 
-fn main() {
-  let mask = generate_sliding_piece_mask(&28, true, false); // d4 rook
-  
-  for occupancy in generate_occupancies(mask) {
-    println!("{:b}", occupancy);
+fn find_magic_number(square: i32, attack_mask: u64, orthagonal: bool, diagonal: bool) -> u64 {
+  let occupancies = generate_occupancies(&attack_mask);
+  let relevant_bits = count_bits(attack_mask);
+  let mut rng = thread_rng(); // init the rng
+
+  loop {
+    let magic_candidate = random::<u64>() & random::<u64>() & random::<u64>(); // AND 3 different random numbers to reduce the active bits. this is done to hopefully find a smaller candidate
+    let product = attack_mask.wrapping_mul(magic_candidate); // overflows :/
+    if count_bits(product & 0xFF00000000000000) < 6 { // if the number of bits set to 1 are greater than 6, the candidate is too large
+      continue;
+    }
+
+    let mut used_attacks = HashMap::new();
+    let mut fail = false;
+
+    for occupancy in &occupancies {
+      let attack_index = ((occupancy * magic_candidate) >> (64 - relevant_bits)) as usize; // this is the hash function for the key to the attack. https://analog-hors.github.io/site/magic-bitboards/
+      let attacks_bitboard = find_legal_moves(&square, occupancy, orthagonal, diagonal);
+
+      // check for collisions in the hashmap
+      if let Some(existing_attack) = used_attacks.get(&attack_index) { // if there is an attack at this index
+        if *existing_attack != attacks_bitboard { // if the attack_bitboard is different from the position in the hashmap
+          fail = true;
+          break;
+        }
+      } else {
+        used_attacks.insert(attack_index, attacks_bitboard);
+      }
+    }
+
+    if !fail {
+      return magic_candidate;
+    }
   }
+}
+
+fn main() {
+  env::set_var("RUST_BACKTRACE", "1");
+  let square = 28;
+  let rook = (true, false);
+  let mask = generate_sliding_piece_mask(&square, rook.0, rook.1); // d4 rook
+  let magic_number = find_magic_number(square, mask, rook.0, rook.1);
+  println!("{}", magic_number);
 }
